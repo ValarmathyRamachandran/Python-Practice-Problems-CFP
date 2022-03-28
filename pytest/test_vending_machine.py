073cdaf2f4a21241279424d2584bd579652e2a8e
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-03-28 22:36:43 +0530

    [Valarmathy]: refactored code for testing multiple values using pytest

--- pytest/test_vending_machine.py
+++ pytest/test_vending_machine.py
@@ -3,12 +3,10 @@ import vending_machine
 
 
 def test_vending_machine():
-    assert vending_machine.count_currency(1000) == 1
-
-
-def test_vending_machine2():
     assert vending_machine.count_currency(2000) == 2
 
 
-def test_vending_machine3():
-    assert vending_machine.count_currency(3000) == 3
+def test_vending_machine_2():
+    assert vending_machine.count_currency(2500) == 3
+
+
--- pytest/vending_machine.py
+++ pytest/vending_machine.py
@@ -9,22 +9,22 @@ def count_currency(amount):
 
     print("Currency Count -> ")
 
-    totalNotes = 0
+    totalNotes = 1
     for note, number_of_notes in zip(notes, noteCounter):
-        #which is an iterator of tuples where the first item in each passed iterator is paired together,
+        # which is an iterator of tuples where the first item in each passed iterator is paired together,
         # and then the second item in each passed iterator are paired together
         if amount >= note:
-            number_of_notes = amount // note
-            amount = amount - number_of_notes * note
-            print(note, " : ", number_of_notes)
-            totalNotes += number_of_notes
-            return totalNotes
-
-    print("minimun number of Notes: ", totalNotes)
+            for note, no_of_notes in zip(notes, noteCounter):
+                if amount >= note:
+                    no_of_notes = amount // note
+                    amount = amount - no_of_notes * note
+                    totalNotes += 1
+                    print(note, " : ", no_of_notes)
+        print(totalNotes)
+        return totalNotes
 
 
 if __name__ == "__main__":
-    count_currency(3500)
-
-
+    count_currency(2000)
+    count_currency(2500)
 
